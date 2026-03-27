8fdb3b85ef6de32dfe4700b1ccbceb9b441ca272
LShpringer <Editor@lshpringer.ru>
2026-03-27 17:36:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -198,12 +198,12 @@ function initEditor() {
         theme: 'snow',
         modules: {
             toolbar: [
-                [{ header: [1, 2, 3, false] }],
-                ['bold', 'italic', 'underline'],
-                [{ list: 'ordered' }, { list: 'bullet' }],
-                ['link'],
-                ['clean'],
-            ]
+    [{ header: [1, 2, 3, false] }],
+    ['bold', 'italic', 'underline'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link', 'image'],
+    ['clean'],
+]
         }
     });
 }
